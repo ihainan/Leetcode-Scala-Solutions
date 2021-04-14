25143ef09f89aab777d1c2e104e5b0393e016194
fjbj <fjbj@cn.ibm.com>
2021-04-14 20:23:56 +0800

    Add solutions

---
+++ src/main/scala/me/ihainan/P783.scala
@@ -0,0 +1,20 @@
+package me.ihainan
+
+object P783 {
+  def minDiffInBST(root: TreeNode): Int = {
+    var ans = Int.MaxValue
+    var prev = -1
+
+    def dfs(node: TreeNode): Unit = {
+      if (node != null) {
+        dfs(node.left)
+        if (prev != -1) ans = ans.min((prev - node.value).abs)
+        prev = node.value
+        dfs(node.right)
+      }
+    }
+
+    dfs(root)
+    ans
+  }
+}
