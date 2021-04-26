2c6f03f813749956184df30fa739f488c964e096
fjbj <fjbj@cn.ibm.com>
2021-04-27 07:49:11 +0800

    Add solutions

--- src/main/scala/me/ihainan/O11.scala
+++ src/main/scala/me/ihainan/O11.scala
@@ -2,15 +2,16 @@ package me.ihainan
 
 object O11 {
   def minArray(nums: Array[Int]): Int = {
-    nums(find(0, nums.length - 1, nums))
+    find(0, nums.length - 1, nums)
   }
 
+  @annotation.tailrec
   def find(l: Int, r: Int, nums: Array[Int]): Int = {
-    if (l == r) l
+    if (l == r) nums(l)
     else {
       val mid = (l + r) >>> 1
-      if (nums(mid) > nums(r)) find(mid + 1, r, nums)
-      else if (nums(mid) < nums(r)) find(l, mid, nums)
+      if (nums(mid) < nums(r)) find(l, mid, nums)
+      else if (nums(mid) > nums(r)) find(mid + 1, r, nums)
       else find(l, r - 1, nums)
     }
   }
--- src/main/scala/me/ihainan/O12.scala
+++ src/main/scala/me/ihainan/O12.scala
@@ -2,26 +2,27 @@ package me.ihainan
 
 object O12 {
   def exist(board: Array[Array[Char]], word: String): Boolean = {
-    board.indices.foreach { x =>
-      board.head.indices.foreach { y =>
-        if (dfs(board, 0, x, y, word)) return true
+    val (m, n) = (board.length, board.head.length)
+    (0 until m).foreach { i =>
+      (0 until n).foreach { j =>
+        if (board(i)(j) == word.head) {
+          if (dfs(board, 0, i, j, "", word)) return true
+        }
       }
     }
     false
   }
 
-  val mx = Array(1, 0, -1, 0)
-  val my = Array(0, 1, 0, -1)
+  val mx = Array(0, 1, 0, -1)
+  val my = Array(1, 0, -1, 0)
 
-  def dfs(board: Array[Array[Char]], i: Int, x: Int, y: Int, word: String): Boolean = {
-    if (i == word.length) true
-    else if (x < 0 || y < 0 || x >= board.length || y >= board.head.length) false
-    else if (board(x)(y) != word(i)) false
+  def dfs(board: Array[Array[Char]], dep: Int, x: Int, y: Int, current: String, target: String): Boolean = {
+    if (dep == target.length) current == target
+    else if (x < 0 || x >= board.length || y < 0 || y >= board.head.length || board(x)(y) == 'X' || board(x)(y) != target(dep)) false
     else {
-      val tmp = board(x)(y)
-      board(x)(y) = 0
-      val ans = (0 until 4).exists { j => dfs(board, i + 1, x + mx(j), y + my(j), word) }
-      board(x)(y) = tmp
+      board(x)(y) = 'X'
+      val ans = (0 until 4).exists { i => dfs(board, dep + 1, x + mx(i), y + my(i), current + target(dep), target) }
+      board(x)(y) = target(dep)
       ans
     }
   }
