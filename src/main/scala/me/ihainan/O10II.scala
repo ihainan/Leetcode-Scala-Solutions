be2307bcf09e678d0911cbf2a93c7de92a2eea9b
fjbj <fjbj@cn.ibm.com>
2021-04-26 08:39:35 +0800

    Add solutions

--- src/main/scala/me/ihainan/O10II.scala
+++ src/main/scala/me/ihainan/O10II.scala
@@ -2,15 +2,12 @@ package me.ihainan
 
 object O10II {
   def numWays(n: Int): Int = {
-    if (n == 0) 1
-    else {
-      var (a, b) = (1, 2)
-      (2 to n).foreach { _ =>
-        val sum = (a + b) % 1000000007
-        a = b
-        b = sum
-      }
-      a
+    var (a, b) = (1, 2)
+    (1 until n).foreach { _ =>
+      val tmp = (a + b) % 1000000007
+      a = b
+      b = tmp
     }
+    a
   }
 }
