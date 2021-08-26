8426b5657b19ff0e44a0301cd70a6cf203978200
ihainan <ihainan72@gmail.com>
2021-08-27 07:37:00 +0800

    Add solutions

--- src/main/scala/Solution.scala
+++ src/main/scala/Solution.scala
@@ -1,6 +1,14 @@
 object Solution {
-  def escapeGhosts(ghosts: Array[Array[Int]], target: Array[Int]): Boolean = {
-    println("Hello World")
-    true
+  def solve(n: Int): Int = {
+    var dp = Array(0, 0, 1, 1)
+    (1 until n).foreach { _ =>
+      val tp = Array(dp(1), dp(2), dp(3), dp(0) + dp(1))
+      dp = tp
+    }
+    dp(0)
+  }
+
+  def main(args: Array[String]): Unit = {
+    (1 to 10).foreach(i => println(i, solve(i)))
   }
 }
