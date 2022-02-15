a1b7c6d42cfc9b8c5959ec1e3796b50fa2908eae
fjbj <fjbj@cn.ibm.com>
2022-02-15 08:41:25 +0800

    Add solutions

--- src/main/scala/me/ihainan/P1380.scala
+++ src/main/scala/me/ihainan/P1380.scala
@@ -1,14 +1,15 @@
 package me.ihainan
 
 object P1380 {
-  def luckyNumbers (matrix: Array[Array[Int]]): List[Int] = {
+  def luckyNumbers(matrix: Array[Array[Int]]): List[Int] = {
     val (m, n) = (matrix.length, matrix.head.length)
     val rows = matrix.map(_.min)
     val columns = (0 until n).map(j => (0 until m).map(i => matrix(i)(j)).max)
     val ans = collection.mutable.ListBuffer.empty[Int]
     matrix.indices.foreach { i =>
       matrix(i).indices.foreach { j =>
-        if (matrix(i)(j) == rows(i) && matrix(i)(j) == columns(j)) ans += matrix(i)(j)
+        if (matrix(i)(j) == rows(i) && matrix(i)(j) == columns(j))
+          ans += matrix(i)(j)
       }
     }
 
